30e37aae29346724f0b82b948f5d7c26249b71d0
LaplaceDBen <77884145+LaplaceDBen@users.noreply.github.com>
2021-11-24 11:47:07 +0100

    Aufgabe 37 & 38

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import pandas as pd
+import plotly.express as px
+
+#Aufgabe 37
 df = pd.read_csv(r'.\Summer-Olympic-medals-1976-to-2008.csv', encoding="utf-8")
-print(df)
+print(df.head())
+print(df.info)
+#Aufgabe 38
+#a)
+Land = df.groupby('Country')
+#b)
+Sportart = df.groupby('Sport')
+print(Land.head())
+#c)
+CSdf= df.groupby(['Country', 'Sport']).size().reset_index(name='counts')
+
+fig = px.bar(CSdf, x="Country",
+             y='counts',
+             title="Medal Occurence by Country",
+             barmode='group',
+             height=600
+            )
+
+fig.show()
+
+#Aufgabe 39
+
+#a)
