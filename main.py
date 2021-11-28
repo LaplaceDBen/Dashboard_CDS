afb9407320434d87adb923d62a366a3b4c463beb
LaplaceDBen <77884145+LaplaceDBen@users.noreply.github.com>
2021-11-28 15:47:04 +0100

    Aufgabe 39

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import plotly.express as px
 
 #Aufgabe 37
 df = pd.read_csv(r'.\Summer-Olympic-medals-1976-to-2008.csv', encoding="utf-8")
-print(df.head())
-print(df.info)
+df['Country'] = df['Country'].astype(str)
 #Aufgabe 38
 #a)
 Land = df.groupby('Country')
@@ -21,8 +20,20 @@ fig = px.bar(CSdf, x="Country",
              height=600
             )
 
-fig.show()
+#fig.show()
 
 #Aufgabe 39
-
+df_max = df.groupby(['Country','Year'])['Medal'].count().reset_index(drop=False)
+swiss = df_max[df_max["Country"] == 'Switzerland']
+print(swiss)
 #a)
+fig = px.scatter(x=swiss['Year'], y=swiss['Medal'])
+fig.show()
+df.info()
+#b)
+df = px.data.tips()
+fig = px.histogram(df, x=swiss['Medal'])
+fig.show()
+#c)
+fig = px.line(x=swiss['Year'], y=swiss['Medal'], title='Switzerland Medal per Year')
+fig.show()
