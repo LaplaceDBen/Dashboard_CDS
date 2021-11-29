709b7c8b96770ef9b57964ae100285f02bb1337e
RaphaBru <raphaelbrunold@hotmail.com>
2021-11-29 22:12:30 +0100

    Paar Apassiga, lauft aber nonid. Wird wohl nid pushd!

--- Dashboard.py
+++ Dashboard.py
@@ -5,7 +5,9 @@ import plotly.graph_objects as go
 import numpy as np
 import dash
 from dash import dcc
-import dash_html_components as html
+from dash import html
+from dash.dependencies import Input, Output
+
 df = pd.read_csv(r'.\Summer-Olympic-medals-1976-to-2008.csv', encoding="utf-8")
 df1 = pd.read_csv(r'.\summary.csv', encoding="utf-8")
 df.info()
@@ -23,17 +25,47 @@ colors = {
 fig = px.bar(df1, x=df1['Year'], y=[df1['Bronze'],df1['Silver'],df1['Gold']], color_discrete_map={'Bronze': 'orange', 'Silver': 'silver', 'Gold':'gold'}, title="By Country")
 
 app.layout = html.Div(children=[
-    html.H1(children='Oympics'),
+    html.H1('Oympics',style={'text-align': 'center'}),
 
-    html.Div(children='''
-        Dashboard_Brunold_Rusconi.
+    html.Div('''
+        Dashboard by Brunold & Rusconi
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
-    )
+    ),
+
+    # Raffi: Dropdown (sollte gehen)
+    dcc.Dropdown(id="auswahlLand",
+                 options=[
+                     {"label": "Switzerland", "value": "Switzerland"},
+                     {"label": "Germany", "value": "Germany"},
+                     {"label": "United States", "value": "United States"},
+                     {"label": "Russia", "value": "Russia"}, ],
+                 multi=False,
+                 value="Switzerland",
+                 style={'width': "40%"}
+                 ),
+
+    html.Br(),
+    dcc.Graph(
+        id='dropdownLand',
+        figure={})
 ])
 
+#Raffi: Callback (geht noch gar nicht)
+@app.callback(
+    [Output(component_id='dropdownLand', component_property='figure')],
+    [Input(component_id='auswahlLand', component_property='value')]
+)
+def update_graph(auswahl):
+    container = "Your selection was: {}".format(auswahl)
+    dff = df1.copy()
+    dff = dff[dff["Country_Code"] == auswahl]
+
+    fig = px.bar(df1, x=df1['Year'], y=[df1['Bronze'], df1['Silver'], df1['Gold']], color_discrete_map={'Bronze': 'orange', 'Silver': 'silver', 'Gold': 'gold'}, title="By Country")
+
+    return container, fig
 
 app.run_server(debug=True)
