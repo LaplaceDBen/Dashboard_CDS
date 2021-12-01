4c054cdad4521072dad67ba4657f3f35ae6347bb
RaphaBru <raphaelbrunold@hotmail.com>
2021-12-01 19:47:18 +0100

    Dashboard aufgeräumt, Jahre(x-Achse) in 4-er Abstände skaliert, Olympia Logo eingefügt Läuft einwandfrei

--- Dashboard.py
+++ Dashboard.py
@@ -13,7 +13,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv(r'.\Summer-Olympic-medals-1976-to-2008.csv', encoding="utf-8")
-df1 = pd.read_csv(r'.\summary.csv', encoding="utf-8")
+df1 = pd.read_csv(r'.\summary.csv', encoding="utf-8") #Zusammenfassung des grossen Datensatzes
 #df.info()
 #df1.info()
 
@@ -22,25 +22,23 @@ colors = {
     'text': '#00008B'
 }
 
-#Funktionnierender Plot von Benito
+# #Funktionnierender Plot für Tests
 # fig = px.bar(df1, x=df1['Year'], y=[df1['Bronze'],df1['Silver'],df1['Gold']], color_discrete_map={'Bronze': 'orange', 'Silver': 'silver', 'Gold':'gold'}, title="By Country")
 
-#Leicht abgewandelte Form des Plots, funktuionniert
-fig = px.bar(df1, x='Year', y=['Bronze', 'Silver', 'Gold'],
-             color_discrete_map={'Bronze': 'orange', 'Silver': 'silver', 'Gold': 'gold'},
-             title="So soll es dann aussehen")
-
 app.layout = html.Div([
-    html.H1('Oympics', style= {'text-align':'center'}),
+    html.H1('Oympics', style = {'text-align':'center'}),
 
-    html.Div('''
-        Dashboard by Brunold & Rusconi
-    '''),
+    html.Div(
+        html.Img(src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Olympic_rings_without_rims.svg/1920px-Olympic_rings_without_rims.svg.png',width=120, alt="Olympic Rings"), style = {'text-align':'center'}
+    ),
 
+    html.Div(
+        "Dashboard by Brunold & Rusconi"
+    ),
 
     html.Br(),
 
-    #Dropdown (Raffi)
+    #Dropdown
     dcc.Dropdown(id='dropdownCountry',
                  options = [{'label': i, 'value': i} for i in df1['Country'].unique()],
                  multi = False,
@@ -48,13 +46,13 @@ app.layout = html.Div([
                  style = {"width": "40%"}
                  ),
 
-    dcc.Graph(id='countryplot', figure = {}),
+    dcc.Graph(id='countryplot', figure = {}), #Plot abbilden. in {} kommt der Return von der Callback-Funktion
 
-#Funktionnierender Plot von Benito
-   dcc.Graph(
-       id='example-graph',
-       figure=fig
-   )
+#  Plot ohne Dropdown und Callback zum Schauen ob Plots sauber abgebildet werden
+   # dcc.Graph(
+   #     id='example-graph',
+   #     figure=fig
+   # )
 ])
 
 #Callback (Raffi)
@@ -65,13 +63,13 @@ app.layout = html.Div([
 def update_graph(option_slctd):
     dff = df1.copy()
     dff = dff[dff["Country"] == option_slctd]
-    print(dff.info())
-    print(dff.head()) #Schaut gut aus, Schweiz wird Standardmässig ausgewählt und Daten werden entsprechend angezeigt.
 #Plotly Express (Diagramm definieren)
-    print(type(dff))
     fig = px.bar(dff, x='Year', y=['Bronze', 'Silver', 'Gold'],
                  color_discrete_map={'Bronze': 'orange', 'Silver': 'silver', 'Gold': 'gold'},
+                 labels=dict(value="Number of medals won", year="Year", variable="Medal"),
                  title="Medals won by selected country")
+    fig.update_xaxes(
+        dtick=4)
     return fig,
 
 
