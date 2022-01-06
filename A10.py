d7326853d9b0b4897d13a10d352f615ceb339f45
LaplaceDBen <77884145+LaplaceDBen@users.noreply.github.com>
2022-01-06 10:27:58 +0100

    A10_Aufgabe 47

--- A10.py
+++ A10.py
@@ -40,7 +40,90 @@ def A_46(df):
     )
 
 def A_47(df):
-    print(1)
+    app.layout = html.Div([
+
+        dcc.RadioItems(
+            id='filter-query-read-write',
+            options=[
+                {'label': 'Read filter_query', 'value': 'read'},
+                {'label': 'Write to filter_query', 'value': 'write'}
+            ],
+            value='read'
+        ),
+
+        html.Br(),
+
+        dcc.Input(id='filter-query-input', placeholder='Enter filter query'),
+
+        html.Div(id='filter-query-output'),
+
+        html.Hr(),
+
+        dash_table.DataTable(
+            id='datatable-advanced-filtering',
+            columns=[
+                {'name': i, 'id': i, 'deletable': True} for i in df.columns
+                # omit the id column
+                if i != 'id'
+            ],
+            data=df.to_dict('records'),
+            editable=True,
+            page_action='native',
+            page_size=10,
+            filter_action="native"
+        ),
+        html.Hr(),
+        html.Div(id='datatable-query-structure', style={'whitespace': 'pre'})
+    ])
+
+    @app.callback(
+        Output('filter-query-input', 'style'),
+        Output('filter-query-output', 'style'),
+        Input('filter-query-read-write', 'value')
+    )
+    def query_input_output(val):
+        input_style = {'width': '100%'}
+        output_style = {}
+        if val == 'read':
+            input_style.update(display='none')
+            output_style.update(display='inline-block')
+        else:
+            input_style.update(display='inline-block')
+            output_style.update(display='none')
+        return input_style, output_style
+
+    @app.callback(
+        Output('datatable-advanced-filtering', 'filter_query'),
+        Input('filter-query-input', 'value')
+    )
+    def write_query(query):
+        if query is None:
+            return ''
+        return query
+
+    @app.callback(
+        Output('filter-query-output', 'children'),
+        Input('datatable-advanced-filtering', 'filter_query')
+    )
+    def read_query(query):
+        if query is None:
+            return "No filter query"
+        return dcc.Markdown('`filter_query = "{}"`'.format(query))
+
+    @app.callback(
+        Output('datatable-query-structure', 'children'),
+        Input('datatable-advanced-filtering', 'derived_filter_query_structure')
+    )
+    def display_query(query):
+        if query is None:
+            return ''
+        return html.Details([
+            html.Summary('Derived filter query structure'),
+            html.Div(dcc.Markdown('''```json
+    {}
+    ```'''.format(json.dumps(query, indent=4))))
+        ])
 if __name__ == '__main__':
-    A_46(df)
+    #A_46(df)
+    A_47(df)
     app.run_server(debug=True)
